9f271cbe3331116b3b393e0072a16c75467d09d3
Maksim Buryshynets <maksim.buryshynets@noom.com>
2024-12-09 15:42:54 +0100

    Day 1 - complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,42 @@
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val input = readingInput("Day01.txt") { line ->
+        line.split("   ")
+            .let { (l, r) -> l.toInt() to r.toInt() }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+private fun part1(input: List<Pair<Int, Int>>): Int {
+    val one = PriorityQueue<Int>()
+    val two = PriorityQueue<Int>()
+
+    input.forEach { (l, r) ->
+        one += l
+        two += r
+    }
+    var result = 0
+    while (one.isNotEmpty() && two.isNotEmpty()) {
+        val l = one.poll()
+        val r = two.poll()
+
+        result += abs(l - r)
+    }
+    return result
+}
+
+private fun part2(input: List<Pair<Int, Int>>): Int {
+    val one = ArrayList<Int>()
+    val two = HashMap<Int, Int>().withDefault { 0 }
+
+    input.forEach { (l, r) ->
+        one += l
+        two.merge(r, 1, Int::plus)
+    }
+    return one.sumOf { it * two.getValue(it) }
 }
